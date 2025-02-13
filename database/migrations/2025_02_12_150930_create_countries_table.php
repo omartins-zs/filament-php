3de77b7dbed4d96e12a255cbd7b5f648f33645f7
omartins-zs <gabrielmatheus209@gmail.com>
2025-02-13 11:26:22 -0300

    Ajustando a Migration de Paises

--- database/migrations/2025_02_12_150930_create_countries_table.php
+++ database/migrations/2025_02_12_150930_create_countries_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->string('name');
+            $table->char('code');
+            $table->char('phonecode');
             $table->timestamps();
         });
     }
