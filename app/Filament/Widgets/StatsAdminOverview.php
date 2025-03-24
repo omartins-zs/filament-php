2a7368323b1872c2482964dd6ac21a205ba85675
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-24 11:07:29 -0300

    Add Colors no Widgets Stats

--- app/Filament/Widgets/StatsAdminOverview.php
+++ app/Filament/Widgets/StatsAdminOverview.php
@@ -12,13 +12,16 @@ class StatsAdminOverview extends BaseWidget
         return [
             Stat::make('Unique views', '192.1k')
                 ->description('32k increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up'),
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
             Stat::make('Bounce rate', '21%')
-                ->description('7% decrease')
-                ->descriptionIcon('heroicon-m-arrow-trending-down'),
+                ->description('7% increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-down')
+                ->color('danger'),
             Stat::make('Average time on page', '3:12')
                 ->description('3% increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up'),
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
         ];
     }
 }
