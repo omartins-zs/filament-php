3f464ab147264f3e08e8e7ba3b80a4ea5532de53
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-24 11:05:37 -0300

    Add Stats e Description no Teste de Widgets

--- app/Filament/Widgets/StatsAdminOverview.php
+++ app/Filament/Widgets/StatsAdminOverview.php
@@ -10,9 +10,15 @@ class StatsAdminOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Unique views', '192.1k'),
-            Stat::make('Bounce rate', '21%'),
-            Stat::make('Average time on page', '3:12'),
+            Stat::make('Unique views', '192.1k')
+                ->description('32k increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up'),
+            Stat::make('Bounce rate', '21%')
+                ->description('7% decrease')
+                ->descriptionIcon('heroicon-m-arrow-trending-down'),
+            Stat::make('Average time on page', '3:12')
+                ->description('3% increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up'),
         ];
     }
 }
