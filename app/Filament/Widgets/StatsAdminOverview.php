885af0793cee56f1e4287a69e4a0e28796f62f56
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-24 11:15:16 -0300

    Ajustando Title e dados do Stats Admin

--- app/Filament/Widgets/StatsAdminOverview.php
+++ app/Filament/Widgets/StatsAdminOverview.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Employee;
+use App\Models\Team;
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,17 +13,17 @@ class StatsAdminOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Unique views', '192.1k')
-                ->description('32k increase')
+            Stat::make('Users', User::query()->count())
+                ->description('All users from the database')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
-            Stat::make('Bounce rate', '21%')
-                ->description('7% increase')
+            Stat::make('Teams', Team::query()->count())
+                ->description('All teams from the database')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('danger'),
-            Stat::make('Average time on page', '3:12')
-                ->description('3% increase')
+            Stat::make('Employees', Employee::query()->count())
+                ->description('All employees from the database')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success'),
         ];
