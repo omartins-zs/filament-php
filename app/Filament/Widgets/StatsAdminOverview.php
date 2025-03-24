83ae1c42bef4d65bbbdad0becc7b89cbf2be4f7e
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-24 11:09:44 -0300

    Add Grafico(Chart) no Widget

--- app/Filament/Widgets/StatsAdminOverview.php
+++ app/Filament/Widgets/StatsAdminOverview.php
@@ -13,6 +13,7 @@ class StatsAdminOverview extends BaseWidget
             Stat::make('Unique views', '192.1k')
                 ->description('32k increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
             Stat::make('Bounce rate', '21%')
                 ->description('7% increase')
